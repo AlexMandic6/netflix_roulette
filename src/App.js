a147e80e3470e017588c673adc2de383284b68d1
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-09-03 17:48:34 +0200

    removed unnecessary files, added counter component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Counter from "./Counter/Counter";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Netflix Roulette</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-}
+const App = () => {
+	return <Counter />;
+};
 
 export default App;
