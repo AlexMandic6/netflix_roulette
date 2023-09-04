03b62c0dd15f0a1996ee739fb0760e751e4a42c3
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-09-04 16:28:48 +0200

    added overview component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Counter from "./Counter/Counter";
 import Search from "./Search/Search";
+import Overview from "./Overview/Overview";
 
 const App = () => {
 	return (
 		<>
 			<Counter />
 			<Search />
+			<Overview />
 		</>
 	);
 };
---
+++ src/Overview/Overview.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Overview.css";
+
+const Overview = () => {
+	return <div className="overview">Overview</div>;
+};
+
+export default Overview;
