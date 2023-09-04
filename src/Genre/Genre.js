dadf70a2a2992452eb9b26687399d1f410679a4d
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-09-04 20:59:53 +0200

    added genre section

---
+++ src/Genre/Genre.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Genre.css";
+
+const Genre = ({ genre }) => {
+	return <button className="genre">{genre}</button>;
+};
+
+export default Genre;
--- src/Overview/Overview.js
+++ src/Overview/Overview.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Overview.css";
+import Genres from "../Genres/Genres";
 
 const Overview = () => {
-	return <div className="overview">Overview</div>;
+	return (
+		<div className="overview">
+			<Genres />
+		</div>
+	);
 };
 
 export default Overview;
