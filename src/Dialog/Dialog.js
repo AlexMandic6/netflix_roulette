e5b55c0ccb1d563ff838c4c0570942d80aed5a02
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-10-05 14:25:17 +0200

    removed from package, added focus-trap-react to the dialog

--- src/Dialog/Dialog.js
+++ src/Dialog/Dialog.js
@@ -2,17 +2,21 @@ import "./Dialog.css";
 import Button from "../Button/Button";
 import CloseIcon from "../CloseIcon/CloseIcon";
 
+import FocusTrap from "focus-trap-react";
+
 const Dialog = ({ children, title, closePortal }) => {
 	return (
-		<div className="dialog">
-			<h2 className="dialog__title">{title}</h2>
-			<Button
-				onClick={closePortal}
-				buttonClass="dialog__close-btn"
-				buttonText={<CloseIcon />}
-			/>
-			{children}
-		</div>
+		<FocusTrap>
+			<div className="dialog">
+				<h2 className="dialog__title">{title}</h2>
+				<Button
+					onClick={closePortal}
+					buttonClass="dialog__close-btn"
+					buttonText={<CloseIcon />}
+				/>
+				{children}
+			</div>
+		</FocusTrap>
 	);
 };
 
