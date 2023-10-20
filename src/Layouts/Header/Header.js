a814e4bddd9361fe3210d52640c7370ed099ac8e
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-10-20 13:49:02 +0200

    cleaned up header component

--- src/Layouts/Header/Header.js
+++ src/Layouts/Header/Header.js
@@ -1,18 +1,13 @@
-import React, { useState } from "react";
 import SearchForm from "SearchForm/SearchForm";
-import SearchButton from "SearchButton/SearchButton";
 import Logo from "Components/Logo/Logo";
 import Button from "Components/Button/Button";
 import MovieForm from "MovieForm/MovieForm";
 import { PortalWithState } from "react-portal";
-
 import "./Header.css";
 
-const Header = () => {
-	const [searchTerm, setSearchTerm] = useState("");
-
+const Header = ({ onSearchedMovie }) => {
 	const onSearch = (searchTerm) => {
-		setSearchTerm(searchTerm);
+		onSearchedMovie(searchTerm);
 	};
 	return (
 		<div className="search">
@@ -39,10 +34,7 @@ const Header = () => {
 			</div>
 			<div className="search__form__wrapper">
 				<h1 className="search__title">Find your movie</h1>
-				<div className="search__form">
-					<SearchForm searchTerm={searchTerm} onSearch={onSearch} />
-					<SearchButton searchTerm={searchTerm} onSearch={onSearch} />
-				</div>
+				<SearchForm onSearch={onSearch} />
 			</div>
 		</div>
 	);
