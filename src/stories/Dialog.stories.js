253a8ceb525167c82bc099c90cd58624af5e5002
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-10-30 15:09:29 +0100

    added add, edit and delete movie story

--- src/stories/Dialog.stories.js
+++ src/stories/Dialog.stories.js
@@ -1,5 +1,7 @@
 import Dialog from "components/Dialog/Dialog";
 import { PortalWithState } from "react-portal";
+import Button from "components/Button/Button";
+import MovieForm from "components/MovieForm/MovieForm";
 
 export default {
 	title: "Components/Dialog",
@@ -7,30 +9,61 @@ export default {
 };
 
 const Template = ({ ...args }) => (
-	<PortalWithState closeOnOutsideClick closeOnEsc>
+	<PortalWithState closeOnEsc>
 		{({ openPortal, closePortal, isOpen, portal }) => (
 			<>
-				<button onClick={openPortal} className="dialog-btn">
-					{args.dialogBtnText}
-				</button>
+				<Button
+					onClick={openPortal}
+					buttonClass="dialog-btn"
+					buttonText={args.dialogBtnText}
+				/>
 				{isOpen &&
 					portal(
-						<Dialog
-							title={args.dialogTitle}
-							closePortal={closePortal}
-						>
-							<p className="dialog__text">{args.dialogText}</p>
-						</Dialog>
+						args.formDialog ? (
+							<MovieForm
+								closePortal={closePortal}
+								title={args.dialogTitle}
+							/>
+						) : (
+							<Dialog
+								title={args.dialogTitle}
+								closePortal={closePortal}
+							>
+								<p className="dialog__description">
+									{args.dialogDescription}
+								</p>
+								<Button
+									buttonType="button"
+									buttonText={args.dialogBtnSubmitText}
+									buttonClass="btn-primary"
+								/>
+							</Dialog>
+						)
 					)}
 			</>
 		)}
 	</PortalWithState>
 );
 
-export const Default = Template.bind({});
-Default.args = {
-	dialogBtnText: "Open Dialog",
-	dialogTitle: "This is a simple dialog.",
-	dialogText:
-		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus repellendus obcaecati asperiores voluptatem iste a culpa.",
+export const AddMovie = Template.bind({});
+AddMovie.args = {
+	dialogBtnText: "+ Add movie",
+	dialogTitle: "Add Movie",
+	formDialog: true,
+};
+
+export const EditMovie = Template.bind({});
+EditMovie.args = {
+	dialogBtnText: "Edit Movie",
+	dialogTitle: "Edit Movie",
+	formDialog: true,
+};
+
+export const DeleteMovie = Template.bind({});
+DeleteMovie.args = {
+	dialogBtnText: "Delete Movie",
+	dialogTitle: "Add Movie",
+	dialogDescription: "Are you sure you want to delete this movie?",
+	dialogBtnSubmitText: "Confirm",
+	formDialog: false,
 };
