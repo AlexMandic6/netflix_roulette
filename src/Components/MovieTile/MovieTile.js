6f01ee5680c5a6e22070dc172937ded8dd44acb0
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-10-31 16:51:37 +0100

    dropdown on movie hover

--- src/Components/MovieTile/MovieTile.js
+++ src/Components/MovieTile/MovieTile.js
@@ -1,8 +1,12 @@
+import React, { useState } from "react";
 import "./MovieTile.css";
+import Dropdown from "components/Dropdown/Dropdown";
 import extractYearFromDate from "utils/extractYearFromDate";
 import formatGenres from "utils/formatGenres";
 
 const MovieTile = ({ movieData, onMoveDetail }) => {
+	const [isShown, setIsShown] = useState(false);
+
 	const fallbackPosterUrl =
 		"https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg";
 
@@ -13,16 +17,23 @@ const MovieTile = ({ movieData, onMoveDetail }) => {
 
 	return (
 		<>
-			<div onClick={handleMovieTileClick} className="poster">
-				<img
-					className="poster__img"
-					src={movieData?.poster_path}
-					onError={(e) => {
-						e.target.src = fallbackPosterUrl; // Set fallback URL if the original poster_path URL fails to load
-						e.target.classList.add("poster__fallback");
-					}}
-					alt={movieData?.title}
-				/>
+			<div
+				className="poster"
+				onMouseEnter={() => setIsShown(true)}
+				onMouseLeave={() => setIsShown(false)}
+			>
+				<div className="poster__img">
+					{isShown && <Dropdown />}
+					<img
+						src={movieData?.poster_path}
+						onClick={handleMovieTileClick}
+						onError={(e) => {
+							e.target.src = fallbackPosterUrl;
+							e.target.classList.add("poster__fallback");
+						}}
+						alt={movieData?.title}
+					/>
+				</div>
 				<div className="poster__header">
 					<h3 className="poster__title">
 						{movieData?.title || `No Title Available.`}
