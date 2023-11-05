4ecee5aaeeba5d9c7997b9aeb332c995788df0a6
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-11-05 18:28:26 +0100

    commented out Counter component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import Counter from "components/Counter/Counter";
+// import Counter from "components/Counter/Counter";
 import MovieListPage from "pages/MovieListPage/MovieListPage";
 import Footer from "layouts/Footer/Footer";
 
 const App = () => {
 	return (
 		<>
-			<Counter initialCount={0} />
+			{/* <Counter initialCount={0} /> */}
 			<MovieListPage />
 			<Footer />
 		</>
