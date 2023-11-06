981ab0b8f5b1d31f804554606ecbde97d1d39a48
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-11-06 23:32:29 +0100

    added root router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
+import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "error-page";
+import MovieListPage from "pages/MovieListPage/MovieListPage";
+
+import Header from "layouts/Header/Header";
+import MovieDetails from "components/MovieDetails/MovieDetails";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <MovieListPage />,
+		errorElement: <ErrorPage />,
+
+		children: [
+			{
+				path: "/",
+				index: true,
+				element: <Header />,
+			},
+			{
+				path: "/:movieId",
+				element: <MovieDetails />,
+			},
+		],
+	},
+]);
+
+root.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
