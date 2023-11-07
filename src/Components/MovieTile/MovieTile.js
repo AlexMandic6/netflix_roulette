fd22a7446da357675def08f8716df67d67a052a7
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-11-07 15:17:42 +0100

    using navLink

--- src/Components/MovieTile/MovieTile.js
+++ src/Components/MovieTile/MovieTile.js
@@ -3,15 +3,16 @@ import "./MovieTile.css";
 import Dropdown from "components/Dropdown/Dropdown";
 import extractYearFromDate from "utils/extractYearFromDate";
 import formatGenres from "utils/formatGenres";
+import { NavLink } from "react-router-dom";
 
-const MovieTile = ({ movieData, onMoveDetail }) => {
+const MovieTile = ({ movieData }) => {
+	const { id } = movieData;
 	const [isShown, setIsShown] = useState(false);
 
 	const fallbackPosterUrl =
 		"https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg";
 
-	const handleMovieTileClick = () => {
-		onMoveDetail(movieData);
+	const scrollToMovieDetail = () => {
 		window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 	};
 
@@ -24,15 +25,16 @@ const MovieTile = ({ movieData, onMoveDetail }) => {
 			>
 				<div className="poster__img">
 					{isShown && <Dropdown />}
-					<img
-						src={movieData?.poster_path}
-						onClick={handleMovieTileClick}
-						onError={(e) => {
-							e.target.src = fallbackPosterUrl;
-							e.target.classList.add("poster__fallback");
-						}}
-						alt={movieData?.title}
-					/>
+					<NavLink to={`/${id}`} onClick={scrollToMovieDetail}>
+						<img
+							src={movieData?.poster_path}
+							onError={(e) => {
+								e.target.src = fallbackPosterUrl;
+								e.target.classList.add("poster__fallback");
+							}}
+							alt={movieData?.title}
+						/>
+					</NavLink>
 				</div>
 				<div className="poster__header">
 					<h3 className="poster__title">
