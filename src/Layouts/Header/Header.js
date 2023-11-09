405eb066c91bf540b385a8c3b43ffd7ab2a20d1c
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-11-09 22:52:14 +0100

    preserving search query when navigating to add movie form

--- src/Layouts/Header/Header.js
+++ src/Layouts/Header/Header.js
@@ -3,35 +3,26 @@ import Logo from "components/Logo/Logo";
 import Button from "components/Button/Button";
 import MovieForm from "components/MovieForm/MovieForm";
 import { PortalWithState } from "react-portal";
+import { Outlet } from "react-router-dom";
 import "./Header.css";
+import { NavLink, useLocation, createSearchParams } from "react-router-dom";
 
 const Header = () => {
+	const { search } = useLocation();
+	const url = createSearchParams(search).toString();
+	const finalUrl = `/new?${url}`;
 	return (
 		<div className="search">
 			<div className="search__logo">
 				<Logo />
-				<PortalWithState closeOnEsc>
-					{({ openPortal, closePortal, isOpen, portal }) => (
-						<>
-							<Button
-								onClick={openPortal}
-								buttonClass="dialog-btn"
-								buttonText="+ Add movie"
-							/>
-							{isOpen &&
-								portal(
-									<MovieForm
-										closePortal={closePortal}
-										title="Add Movie"
-									/>
-								)}
-						</>
-					)}
-				</PortalWithState>
+				<NavLink to={finalUrl} className="dialog-btn">
+					+ Add movie
+				</NavLink>
 			</div>
 			<div className="search__form__wrapper">
 				<h1 className="search__title">Find your movie</h1>
 				<SearchForm />
+				<Outlet />
 			</div>
 		</div>
 	);
