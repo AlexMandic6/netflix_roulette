48d64399bf932c24fac905905874ca140811f3bf
Aleksandar Mandic <aleksandar.mndc@gmail.com>
2023-11-09 22:52:28 +0100

    created add movie form

---
+++ src/Components/AddMovieForm/AddMovieForm.js
@@ -0,0 +1,27 @@
+import MovieForm from "components/MovieForm/MovieForm";
+import { PortalWithState } from "react-portal";
+import { Navigate, useLocation, createSearchParams } from "react-router-dom";
+
+const AddMovieForm = () => {
+	const { search } = useLocation();
+	const url = createSearchParams(search).toString();
+	const finalUrl = `/?${url}`;
+	return (
+		<PortalWithState closeOnEsc defaultOpen>
+			{({ closePortal, isOpen, portal }) => (
+				<>
+					{!isOpen && <Navigate to={finalUrl} replace={true} />}
+					{isOpen &&
+						portal(
+							<MovieForm
+								closePortal={closePortal}
+								title="Add Movie"
+							/>
+						)}
+				</>
+			)}
+		</PortalWithState>
+	);
+};
+
+export default AddMovieForm;
